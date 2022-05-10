7891cc9b1a453103a1b25be0ec8ca2114cdcc013
Trang <trang@example.com>
2022-05-10 22:45:53 +0700

    vd4

--- app/src/main/java/com/example/todoapp/RegistrationActivity.java
+++ app/src/main/java/com/example/todoapp/RegistrationActivity.java
@@ -2,16 +2,43 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+
+
+    private EditText RegEmail,RegPwd;
+    private Button RegBtn;
+    private TextView RegQn;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_registration);
+
+        toolbar = findViewById(R.id.RegisterToolbar);
+
+        RegEmail = findViewById(R.id.RegisterEmail);
+        RegPwd = findViewById(R.id.RegisterPassword);
+        RegBtn = findViewById(R.id.RegisterButton);
+        RegQn = findViewById(R.id.loginPageQuestion);
+
+        RegQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
